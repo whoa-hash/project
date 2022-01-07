9b2a409ed9dfb7ba9805d0a06ffc81389632e112
whoa-hash <59946519+whoa-hash@users.noreply.github.com>
2022-01-07 12:38:44 -0500

    Delete memory_alloc.c

--- memory_alloc.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-/* https://stackoverflow.com/questions/6618921/calling-a-function-from-another-file-in-the-same-directory-in-c
-source above for using other files across files in c
-*/
-int main(){
-    // do the work TODO()
-    printf("Let\'s see what happens");
-}
